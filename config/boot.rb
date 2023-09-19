d80bf6fd9fbcaafcbf4c1cad7eea355bc62d65aa
Paul Bob <paul.ionut.bob@gmail.com>
2023-09-19 17:16:16 +0300

    comment ENV["AVO_IN_DEVELOPMENT"] = "1"

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,5 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
-ENV["AVO_IN_DEVELOPMENT"] = "1"
+# ENV["AVO_IN_DEVELOPMENT"] = "1"
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
