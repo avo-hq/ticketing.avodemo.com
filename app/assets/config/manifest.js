181f8aa2488cb3a621b06d689168e8eb54fb7dee
Paul Bob <paul.ionut.bob@gmail.com>
2023-09-20 18:44:17 +0300

    add avo to manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_directory ../stylesheets/avo .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link avo.base.js
