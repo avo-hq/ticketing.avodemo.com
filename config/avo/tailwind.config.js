f65c62b98c36ae2d71a0fa58de58d71f53e8909a
Paul Bob <paul.ionut.bob@gmail.com>
2025-03-05 18:31:27 +0200

    update to tw4

--- config/avo/tailwind.config.js
+++ config/avo/tailwind.config.js
@@ -1,12 +1,5 @@
 const avoPreset = require('../../tmp/avo/tailwind.preset.js')
 
 module.exports = {
-  presets: [avoPreset],
-  content: [
-    ...avoPreset.content,
-    './app/views/**/*.html.erb',
-    './app/components/**/*.{html.erb,rb}',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-  ]
+  presets: [avoPreset]
 }
